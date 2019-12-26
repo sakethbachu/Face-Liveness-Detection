7fc0f05895b4b941ee86b6a906960cbba689ff53
saketh bachu <saketh7000@gmail.com>
2019-12-27 00:43:54 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,7 +13,7 @@ from scipy.spatial import distance as dist
 x = 0
 
 
-# construct the argument parse and parse the arguments
+# construct argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", type=str, required=True,
 	help="path to trained model")
@@ -28,23 +28,22 @@ ap.add_argument("-p", "--shape-predictor", required=True,
 args = vars(ap.parse_args())
 
 
-# load our serialized face detector from disk
-print("[INFO] loading face detector...")
+# loading face detector from the place where we stored it
+print("loading face detector")
 protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+#Loading the caffe model 
 modelPath = os.path.sep.join([args["detector"],
 	"res10_300x300_ssd_iter_140000.caffemodel"])
+#reading data from the model.
 net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-# load the liveness detector model and label encoder from disk
-print("[INFO] loading liveness detector...")
+# loading the liveness detecting module that was trained in the training python script
+print("loading the liveness detector")
 model = load_model(args["model"])
 le = pickle.loads(open(args["le"], "rb").read())
 
-## initialize the video stream and allow the camera sensor to warmup
-#print("[INFO] starting video stream...")
-#vs = VideoStream(src=0).start()
-#time.sleep(2.0)
 
+#determining the facial points that are plotted by dlib
 FULL_POINTS = list(range(0, 68))  
 FACE_POINTS = list(range(17, 68))  
    
@@ -59,12 +58,14 @@ MOUTH_INNER_POINTS = list(range(61, 68))
    
 EYE_AR_THRESH = 0.30 
 EYE_AR_CONSEC_FRAMES = 2  
-   
+
+#initializing the parameters
 COUNTER_LEFT = 0  
 TOTAL_LEFT = 0  
    
 COUNTER_RIGHT = 0  
 TOTAL_RIGHT = 0 
+#defining a function for calculating ear and then comparing with the confidence parametrs
 
 def eye_aspect_ratio(eye):
     
@@ -73,13 +74,17 @@ def eye_aspect_ratio(eye):
     C = dist.euclidean(eye[0], eye[3])  
     ear = (A + B) / (2.0 * C)  
     return ear 
+
+#loading the predictor for predicting
 detector = dlib.get_frontal_face_detector()  
+
+#accessing the shape predictor
 predictor = dlib.shape_predictor(args["shape_predictor"])
-
+#starting the stream
 video_capture = cv2.VideoCapture(0)  
-
+#looping over frames
 while True:
-    #checkpoint for mistake 1
+    #checkpoint 1
     ret, frame = video_capture.read()
     if ret:
         
@@ -99,7 +104,9 @@ while True:
             right_eye_hull = cv2.convexHull(right_eye)  
             ear_left = eye_aspect_ratio(left_eye)  
             ear_right = eye_aspect_ratio(right_eye)
-        
+		
+            #calculating blink wheneer the ear value drops down below the threshold
+	
             if ear_left < EYE_AR_THRESH:
                 
                 COUNTER_LEFT += 1
@@ -132,52 +139,54 @@ while True:
             x = TOTAL_LEFT + TOTAL_RIGHT
 
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+    temp = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
 		(300, 300), (104.0, 177.0, 123.0))
-    net.setInput(blob)
+    net.setInput(temp)
     detections = net.forward()
     for i in range(0, detections.shape[2]):
         
         
         confidence = detections[0, 0, i, 2]
             
-            
+          #staisfying the union need of veryfying through ROI and blink detection.  
         if confidence > args["confidence"] and x>10:
             
             
              
-            
+            #detect a bounding box
+	    #take dimensions
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+	    #get the dimensions
             (startX, startY, endX, endY) = box.astype("int")
 
-			# ensure the detected bounding box does fall outside the
-			# dimensions of the frame
+			
             startX = max(0, startX)
             startY = max(0, startY)
             endX = min(w, endX)
             endY = min(h, endY)
 
-			# extract the face ROI and then preproces it in the exact
-			# same manner as our training data
+	# extract the face ROI and then preproces it in the exact
+	# same manner as our training data
             face = frame[startY:endY, startX:endX]
             face = cv2.resize(face, (32, 32))
             face = face.astype("float") / 255.0
             face = img_to_array(face)
             face = np.expand_dims(face, axis=0)
 
-			# pass the face ROI through the trained liveness detector
-			# model to determine if the face is "real" or "fake"
+	#pass the model to determine the liveness
             preds = model.predict(face)[0]
             j = np.argmax(preds)
             label = le.classes_[j]
 
-			# draw the label and bounding box on the frame
+		# tag with the label
+		#tag with the bounding box
             label = "{}: {:.4f}".format(label, preds[j])
             cv2.putText(frame, label, (startX, startY - 10),
 				 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
             cv2.rectangle(frame, (startX, startY), (endX, endY),
 				  (0, 0, 255), 2)
-    cv2.imshow("Frame", frame)
+ #showing the frames and waiting for the key to be pressed
+cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
